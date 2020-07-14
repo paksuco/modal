3f5eb0f3b9f6786ef93c4961adbc39a7e18f7e35
Taha Paksu <taha.paksu@edata.com.tr>
2020-07-14 15:50:48 +0300

    played with styles

--- views/components/modal.blade.php
+++ views/components/modal.blade.php
@@ -1,13 +1,12 @@
 @if($shown && $view)
-<div
-     class="pm-modal-container fixed flex overflow-y-scroll overflow-x-hidden inset-0 items-center justify-center w-screen h-screen"
-     style="z-index: 99999">
-    <div class="paksuco-modal absolute mx-auto my-auto bg-white z-10 rounded text-sm shadow-lg leading-none">
-        <div class="pm-header font-semibold p-4 rounded-t bg-gray-200">
+<div class="pm-modal-container fixed flex overflow-y-scroll overflow-x-hidden inset-0 items-center justify-center w-screen h-screen"
+     style="z-index: 10001">
+    <div class="paksuco-modal absolute mx-auto my-auto bg-white z-10 rounded-lg text-sm shadow leading-none">
+        <div class="pm-header font-semibold p-4 rounded-t bg-gray-200 capitalize">
             {!! $title !!}
         </div>
         <i class="fa fa-times absolute m-4 right-0 top-0 hover:text-gray-700 cursor-pointer" wire:click="close"></i>
-        <div class="pm-body p-4" style="min-width: 400px">
+        <div class="pm-body">
             @livewire($view, ["args" => $args])
         </div>
     </div>
