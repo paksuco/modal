91027bc6ebc89d7244f925c2a64856bc26f851bb
Taha Paksu <taha.paksu@edata.com.tr>
2020-07-20 16:24:11 +0300

    styling

--- views/components/modal.blade.php
+++ views/components/modal.blade.php
@@ -1,6 +1,6 @@
 @if($shown && $view)
 <div class="pm-modal-container fixed flex overflow-y-scroll overflow-x-hidden inset-0 items-center justify-center w-screen h-screen"
-     style="z-index: 10001">
+     style="z-index: 20001">
     <div class="paksuco-modal absolute mx-auto my-auto bg-white z-10 rounded-lg text-sm shadow leading-none">
         <div class="pm-header font-semibold p-4 rounded-t bg-gray-200 capitalize">
             {!! $title !!}
@@ -10,7 +10,7 @@
             @livewire($view, ["args" => $args])
         </div>
     </div>
-    <div class="pm-modal-backdrop bg-opacity-50 bg-black w-screen h-screen absolute inset-0 z-0"></div>
+    <div class="pm-modal-backdrop bg-opacity-50 bg-black w-screen h-screen absolute inset-0 z-0 pointer-events-none"></div>
 </div>
 @else
 <div></div>
